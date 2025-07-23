eb5be82bf45445d952ff71cb0329b8e2a582457e
Paola <paolaarruee@gmail.com>
2025-07-23 15:38:28 -0300

    feat: permissoes cors

--- back/src/server/Server.ts
+++ back/src/server/Server.ts
@@ -1,8 +1,14 @@
 import express from "express";
-
+import cors from "cors";
 import { router } from "./routes";
 
 const server = express();
+
+server.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 server.use(express.json());
 
